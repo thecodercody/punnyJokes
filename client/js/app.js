d6ac57dc03008bcf77b8d86babd47753aa578757
Cody Scholberg <cscholberg@gmail.com>
2016-10-23 15:15:42 -0700

    contact page added

---
+++ client/js/app.js
@@ -0,0 +1,60 @@
+angular.module('pundexApp', ['ngRoute'])
+
+
+.controller('MainCtrl', ['$scope', function($scope){
+
+}])
+
+.config(function($routeProvider, $locationProvider){
+    $routeProvider
+
+      // landing page
+      .when('/', {
+        templateUrl : 'client/pages/home.html',
+        controller : 'homeController'
+      })
+
+      // about page
+      .when('/about', {
+        templateUrl : "client/pages/about.html"
+      })
+
+      // how to pun page
+      .when('/howToPun', {
+        templateUrl : "client/pages/howTo.html"
+      })
+
+      // visual pun page
+      .when('/visualPuns', {
+        templateUrl : "client/pages/visualPuns.html"
+      })
+
+      // double pun page
+      .when('/doublePuns', {
+        templateUrl : "client/pages/doublePuns.html"
+      })
+
+      // religious pun page
+      .when('/religiousPuns', {
+        templateUrl : "client/pages/religiousPuns.html"
+      })
+
+      // list of puns page
+      .when('/listOfPuns', {
+        templateUrl : "client/pages/listOfPuns.html"
+      })
+
+      // contact page
+      .when('/contact', {
+        templateUrl : "client/pages/contact.html"
+      })
+
+      // contact page
+      .when('/thanks', {
+        templateUrl : "client/pages/thanks.html"
+      })
+
+      // all others
+      .otherwise({redirectTo: '/'});
+      $locationProvider.html5Mode(true);
+  });
